fc4066a83d686fd5a0b40a87a3759e5f11e0cd4a
Jiro-884 <166469311+Jiro-884@users.noreply.github.com>
2024-07-19 13:58:59 +0900

    Update sevenz.rs

--- src/extractor/sevenz.rs
+++ src/extractor/sevenz.rs
@@ -91,7 +91,7 @@ mod tests {
         let opts = ExtractorOpts {
             dest: PathBuf::from("results/sevenz"),
             use_archive_name_dir: false,
-            //overwrite: true,
+            overwrite: true,
             v: create_verboser(false),
         };
         match e.perform(file, &opts) {
